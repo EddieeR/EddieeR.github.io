d1d4d93eff449efd9c9161ebb3ec64fe34d62ac7
EddieeR <55113147+EddieeR@users.noreply.github.com>
2020-02-24 14:45:13 -0500

    Add files via upload

---
+++ Alarm Clock/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  
+  setTimeout(function() {
+    $(".box").addClass("alarm");
+  }, 3000);
+});
+$(document).ready(function(){
+  
+  setTimeout(function() {
+    $(".box2").addClass("alarm");
+  }, 3000);
+});
